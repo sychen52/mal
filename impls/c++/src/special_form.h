0f69736663bf0ba823bd9cd250afb5a5cba8c3b7
sychen52 <sychen52@gmail.com>
2022-02-05 15:25:56 -0800

    remove Variable, we use weak_ptr to solve circular dependency of env

--- impls/c++/src/special_form.h
+++ impls/c++/src/special_form.h
@@ -8,28 +8,6 @@ bool is_form(const mal::List& list, const std::string &key);
 
 mal::Type::Ptr eval(mal::ParameterIter &&it, mal::EnvFrame::WeakPtr env);
 
-class Variables {
-public:
-  using Set = std::unordered_set<std::string>;
-  inline void insert_free(const mal::Symbol &s) { free_.insert(s.value()); }
-  inline void insert_free(const Variables::Set& s) { free_.insert(s.begin(), s.end()); }
-  inline void erase_free(const mal::Symbol &s) { free_.erase(s.value()); }
-  inline void erase_free(const Variables::Set& s) { free_.insert(s.begin(), s.end()); }
-  inline void insert_bound(const mal::Symbol &s) { bound_.insert(s.value()); }
-  inline void insert_bound(const Variables::Set& s) { bound_.insert(s.begin(), s.end()); }
-  inline void intersect_bound(const Variables::Set &s);
-  inline void clear_bound() { bound_.clear(); }
-  inline const auto& free() const { return free_; }
-  inline const auto& bound() const { return bound_; }
-  inline void sequential_merge(const Variables &other);
-
-private:
-  std::unordered_set<std::string> free_;
-  std::unordered_set<std::string> bound_;
-};
-
-Variables get_variables(const mal::Type &ast);
-
 /**
  * This series of class does not take ownership of env or ast
  */
@@ -41,7 +19,6 @@ public:
    * This function will return free variables needed by the form and
    * bound variables created in the form.
    */
-  virtual Variables variables() = 0;
 protected:
   mal::EnvFrame::WeakPtr env_;
 };
@@ -50,7 +27,6 @@ class DefBang : public SpecialForm {
 public:
   DefBang(const mal::List &list, mal::EnvFrame::WeakPtr env);
   virtual mal::Type::Ptr operator()() override;
-  virtual Variables variables() override;
 
 private:
   mal::Symbol::Ptr symbol_;
@@ -61,7 +37,6 @@ class LetStar : public SpecialForm {
 public:
   LetStar(const mal::List &list, mal::EnvFrame::WeakPtr env);
   virtual mal::Type::Ptr operator()() override;
-  virtual Variables variables() override;
 
 private:
   mal::Type::Ptr ast_;
@@ -72,7 +47,6 @@ class Do : public SpecialForm {
 public:
   Do(const mal::List &list, mal::EnvFrame::WeakPtr env);
   virtual mal::Type::Ptr operator()() override;
-  virtual Variables variables() override;
 
 private:
   mal::ParameterIter it_;
@@ -82,7 +56,6 @@ class If : public SpecialForm {
 public:
   If(const mal::List &list, mal::EnvFrame::WeakPtr env);
   virtual mal::Type::Ptr operator()() override;
-  virtual Variables variables() override;
 
 private:
   mal::Type::Ptr condition_;
@@ -94,7 +67,6 @@ class FnStar : public SpecialForm {
 public:
   FnStar(const mal::List &list, mal::EnvFrame::WeakPtr env);
   virtual mal::Type::Ptr operator()() override;
-  virtual Variables variables() override;
 
 private:
   mal::List::Ptr parameters_;
